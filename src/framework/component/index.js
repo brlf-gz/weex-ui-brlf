c65abc67eaf1ab6dda79401c03fdff65c42f343a
452MJ <447180005@qq.com>
2018-02-06 17:51:35 +0800

    导出wxc-icon

--- src/framework/component/index.js
+++ src/framework/component/index.js
@@ -1,7 +1,9 @@
 import WxcTouchDiv from './layout/wxc-touch-div.vue';
+import WxcIcon from "./base/wxc-icon/index.vue";
 import WxcProgress from "./base/wxc-progress/index.vue";
 
 export {
     WxcTouchDiv,
+    WxcIcon,
     WxcProgress,
 }
