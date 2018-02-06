79f73e97f9645a3f950fafc59432c71854e1085b
452MJ <447180005@qq.com>
2018-02-06 17:49:54 +0800

    文件目录结构调整

--- src/framework/component/index.js
+++ src/framework/component/index.js
@@ -1,5 +1,5 @@
 import WxcTouchDiv from './layout/wxc-touch-div.vue';
-import WxcProgress from "./base/wxc-progress.vue";
+import WxcProgress from "./base/wxc-progress/index.vue";
 
 export {
     WxcTouchDiv,
