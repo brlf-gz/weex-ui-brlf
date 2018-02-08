34e4e98a325133caf5847211e494b94a07d14789
452MJ <447180005@qq.com>
2018-02-08 14:44:55 +0800

    wxc-progress进度条示例

--- src/demo/component/base/wxc-progress.vue
+++ src/demo/component/base/wxc-progress.vue
@@ -1,14 +1,51 @@
 <template>
-    <div>
-        <wxc-progress percent=60
-                      bar-width=600
-                      bar-height=40
-                      bg-color="#ebebeb"
-                      active-color="#0eadff"
-                      text-color="#000000"
-                      show-info="true"
+    <div class="root">
+        <wxc-progress class="progress"
+                      :percent=percent
+                      :bar-width=barWidth
+                      :bar-height=barHeight
+                      :bg-color=bgColor
+                      :active-color=activeColor
+                      :text-color=textColor
+                      :show-info=showInfo
         >
         </wxc-progress>
+        <div class="divider"></div>
+        <div class="attr-div">
+            <text class="attr-text">进度百分比%</text>
+            <input class="attr-input" placeholder="percent" @change="onChangePercent"/>
+        </div>
+        <div class="divider"></div>
+        <div class="attr-div">
+            <text class="attr-text">宽度</text>
+            <input class="attr-input" placeholder="bar-width" @change="onChangeBarWidth"/>
+        </div>
+        <div class="divider"></div>
+        <div class="attr-div">
+            <text class="attr-text">高度</text>
+            <input class="attr-input" placeholder="bar-height" @change="onChangeBarHeight"/>
+        </div>
+        <div class="divider"></div>
+        <div class="attr-div">
+            <text class="attr-text">背景颜色</text>
+            <input class="attr-input" placeholder="bg-color" @change="onChangeBgColor"/>
+        </div>
+        <div class="divider"></div>
+        <div class="attr-div">
+            <text class="attr-text">进度条颜色</text>
+            <input class="attr-input" placeholder="active-color" @change="onChangeActiveColor"/>
+        </div>
+        <div class="divider"></div>
+        <div class="attr-div">
+            <text class="attr-text">文字颜色</text>
+            <input class="attr-input" placeholder="percent" @change="onChangeTextColor"/>
+        </div>
+        <div class="divider"></div>
+        <div class="attr-div">
+            <text class="attr-text">下载进度（显示/隐藏）</text>
+            <switch @change="onChangeShowInfo"></switch>
+        </div>
+        <div class="divider"></div>
     </div>
 </template>
 
@@ -17,15 +54,77 @@
 
     export default {
 
-        components: {
-            "wxc-progress": WxcProgress
+        data() {
+            return {
+                percent: 0,
+                barWidth: 600,
+                barHeight: 40,
+                bgColor: "#ebebeb",
+                activeColor: "#0eadff",
+                textColor: "#000000",
+                showInfo: false,
+            }
         },
 
-        methods: {}
+        components: {
+            "wxc-progress": WxcProgress,
+        },
+
+        methods: {
+            onChangePercent(input) {
+                this.percent = input.value;
+            },
+            onChangeBarWidth(input) {
+                this.barWidth = input.value;
+            },
+            onChangeBarHeight(input) {
+                this.barHeight = input.value;
+            },
+            onChangeBgColor(input) {
+                this.bgColor = input.value;
+            },
+            onChangeActiveColor(input) {
+                this.activeColor = input.value;
+            },
+            onChangeTextColor(input) {
+                this.textColor = input.value;
+            },
+            onChangeShowInfo(event) {
+                this.showInfo = event.value;
+            },
+        }
     }
 </script>
 
 <style scoped>
+    .progress {
+        margin: 40px;
+    }
 
+    .divider {
+        width: 750px;
+        height: 1px;
+        background-color: #dcdcdc;
+    }
 
+    .attr-div {
+        height: 100px;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 20px;
+    }
+
+    .attr-text {
+        font-size: 36px;
+        color: black;
+    }
+
+    .attr-input {
+        width: 200px;
+        height: 56px;
+        font-size: 36px;
+        color: black;
+        text-align: end;
+    }
 </style>
